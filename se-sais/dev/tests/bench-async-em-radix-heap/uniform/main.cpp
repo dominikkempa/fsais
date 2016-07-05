412f36dae079f9164b086d7242ea68619b058727
Dominik Kempa <dominik.kempa@gmail.com>
2016-07-05 09:13:05 +0300

    Small fixes in benchmarks

--- se-sais/dev/tests/bench-async-em-radix-heap/uniform/main.cpp
+++ se-sais/dev/tests/bench-async-em-radix-heap/uniform/main.cpp
@@ -11,7 +11,7 @@
 
 
 int main() {
-  static const std::uint64_t n_items = (1UL << 30);
+  static const std::uint64_t n_items = (4UL << 30);
   static const std::uint64_t ram_use = (2UL << 30);
   typedef std::uint8_t key_type;
   typedef std::uint64_t value_type;
@@ -20,14 +20,26 @@ int main() {
   std::uint64_t radix_log = 8UL;
   heap_type *heap = new heap_type(radix_log, "/data/radix_heap_tmp", ram_use);
 
+  static const std::uint64_t rand_buffer_size = (4UL << 20);
+  key_type *rand_key_buffer = new key_type[rand_buffer_size];
+  value_type *rand_value_buffer = new value_type[rand_buffer_size];
+  for (std::uint64_t j = 0; j < rand_buffer_size; ++j) {
+    rand_key_buffer[j] = (key_type)utils::random_int64(0L, 255L);
+    rand_value_buffer[j] = (value_type)utils::random_int64(0L, 1000000000000000000L);
+  }
+
   // insertions
   long double start = utils::wclock();
   fprintf(stderr, "Inserting:\n");
+  std::uint64_t rand_buffer_ptr = 0;
   for (std::uint64_t j = 0; j < n_items; ++j) {
     if ((j % 1000) == 0)
       fprintf(stderr, "%.2Lf%%\r", (100.L * j) / n_items);
-    key_type key = (key_type)utils::random_int64(0L, 255L);
-    value_type value = (value_type)utils::random_int64(0L, 1000000000000000000L);
+    key_type key = rand_key_buffer[rand_buffer_ptr];
+    value_type value = rand_value_buffer[rand_buffer_ptr];
+    ++rand_buffer_ptr;
+    if (rand_buffer_ptr == rand_buffer_size)
+      rand_buffer_ptr = 0;
     heap->push(key, value);
   }
   fprintf(stderr, "Finished, I/O = %.2LfMiB/s\n", ((1.L * heap->io_volume()) / (1L << 20)) / (utils::wclock() - start));
@@ -52,5 +64,7 @@ int main() {
   fprintf(stderr, "Runtime: %.2Lfs\n", utils::wclock() - start);
 
   delete heap;
+  delete[] rand_key_buffer;
+  delete[] rand_value_buffer;
 }
 
