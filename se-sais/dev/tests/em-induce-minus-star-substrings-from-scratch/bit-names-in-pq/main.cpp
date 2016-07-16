cb769f1bb298dbe5c9ffdfc6ec62adc0c7351e7a
Dominik Kempa <dominik.kempa@gmail.com>
2016-07-16 09:05:19 +0300

    Small code refactoring

--- se-sais/dev/tests/em-induce-minus-star-substrings-from-scratch/bit-names-in-pq/main.cpp
+++ se-sais/dev/tests/em-induce-minus-star-substrings-from-scratch/bit-names-in-pq/main.cpp
@@ -218,16 +218,14 @@ void test(std::uint64_t n_testcases, std::uint64_t max_length, std::uint64_t rad
             writers[j] = new writer_type(symbols_filenames[j]);
           std::vector<substring> substrings;
           for (std::uint64_t j = 0; j < text_length; ++j) {
-            if (j > 0) {
+            if (j > 0 && suf_type[j - 1]) {
               if (suf_type[j] == 1) {
-                if (suf_type[j - 1] == 1) {
-                  std::string s; s = text[j];
-                  std::uint64_t end = j + 1;
-                  while (end < text_length && suf_type[end] == 1) s += text[end++];
-                  if (end < text_length) s += text[end++];
-                  substrings.push_back(substring(j, s));
-                }
-              } else if (suf_type[j - 1] == 1) {
+                std::string s; s = text[j];
+                std::uint64_t end = j + 1;
+                while (end < text_length && suf_type[end] == 1) s += text[end++];
+                if (end < text_length) s += text[end++];
+                substrings.push_back(substring(j, s));
+              } else {
                 std::string s; s = text[j];
                 substrings.push_back(substring(j, s));
               }
